a794652e1e9e16227427adb576f1407e34a89c2d
Nidhi Pandey <133492844+nidhipandey73@users.noreply.github.com>
2026-03-30 01:55:58 +0530

    Reduce minimum height of element in app.py

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ st.markdown("""
         font-size: 17px;
         text-align: center;
         margin-top: 0.5rem;
-        min-height: 250px;          
+        min-height: 150px;          
         display: flex;              
         align-items: center;       
         justify-content: center;
